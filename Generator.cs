71b8af78c88afbeffa8add6fac6a277e3a900311
VitaliLuca <vitalilucaster@gmail.com>
2020-08-20 21:13:04 +0200

    Initial commit

---
+++ Generator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SaisonCSS
+{
+    class Generator
+    {
+        private string stringToWrite = "";
+        private ItemCollection selectedList;
+        
+        public Generator(ItemCollection items)
+        {
+            this.selectedList = items;
+            getProperties();
+        }
+
+        private async void getProperties()
+        {
+            string jsonString = await FileIO.ReadTextAsync(await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/saisoncss.JSON")));
+            var properties = JsonObject.Parse(jsonString);
+
+            stringToWrite += properties["startDocument"].GetString();
+
+            foreach (StackPanel stack in selectedList.OfType<StackPanel>())                                     //Will look in the ListView for StackPanels
+            {
+                foreach (CheckBox property in stack.Children.OfType<CheckBox>())                                //Will look in the StackPanels for Checkboxes
+                {
+                    switch (property.Name)                                                                      //Switch between properties and if they are checked, add the appropriate CSS code
+                    {
+                        case "accentColor":
+                            if (property.IsChecked == true)
+                            {
+                                stringToWrite += "--accent-color: #900000;}";
+                            }
+                            else
+                            {
+                                stringToWrite += properties[property.Name].GetString();
+                            }
+                            break;
+                        default:
+                            if (property.IsChecked == true)
+                            {
+                                stringToWrite += properties[property.Name].GetString() + "\n";
+                            }
+                            break;
+                    }
+                }
+            }
+
+            ContentDialog dialog = new ContentDialog { Title = "Success", Content = "Generation completed.", CloseButtonText = "Ok" };
+        
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            folderPicker.FileTypeFilter.Add("*");
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            saveFile(folder);
+            dialog.ShowAsync();
+        }
+        private async void saveFile(StorageFolder folder)
+        {
+            string filename = "test.css";
+            StorageFile f = await folder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(f, stringToWrite.ToString());
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SaisonCSS
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = Windows.UI.Color.FromArgb(255, 69, 69, 144);
+            titleBar.ButtonBackgroundColor = Windows.UI.Color.FromArgb(255, 69, 69, 144);
+            titleBar.ButtonForegroundColor = Windows.UI.Colors.White;
+            titleBar.ButtonHoverBackgroundColor = Windows.UI.Color.FromArgb(255, 79, 79, 164);
+            titleBar.ButtonHoverForegroundColor = Windows.UI.Colors.White;
+            titleBar.ButtonPressedBackgroundColor = Windows.UI.Color.FromArgb(255, 110, 110, 255);
+            titleBar.ButtonPressedForegroundColor = Windows.UI.Colors.White;
+            this.InitializeComponent();
+        }
+
+        private async void generatePressed(object sender, RoutedEventArgs e)
+        {
+            
+            Generator generator = new Generator(listView.Items);
+        }
+    }
+}
